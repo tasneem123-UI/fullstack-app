5b8c19a10fd4d3522d1d543a167c31fcd9edd09d
Tasneem Khalid <tasneemakhalid@gmail.com>
2026-08-17 19:57:24 +0300

    add backend

---
+++ backend/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = 5002;
+
+app.get('/', (req, res) => {
+  res.send('🚀 Backend is working!');
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
